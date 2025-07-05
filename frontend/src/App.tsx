1e2da820246cb510d9839a2f6f21a6377a5f4ca6
WesleySDev <wesleydasilvagit@gmail.com>
2025-07-05 14:34:05 -0300

    Mudança de cores no tailwind, e funcionalidade na pages / Dashboard /Login / Register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import LoginPage from './pages/Login'
-import ProtectedRoute from './auth/ProtectedRoute'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/Login";
+
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        {/* Redirect root to dashboard */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
